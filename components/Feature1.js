a2d35c82659a7e68cbbcebaeddf34e1af5a3609d
Sahil Sharma <sahil.sharma@123789.org>
2024-05-27 15:00:45 +0530

    first feature is ready to commit

---
+++ components/Feature1.js
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const Feature1 = (props: Props) => {
+  return (
+    <View>
+      <Text>Feature1</Text>
+    </View>
+  );
+};
+
+export default Feature1;
+
+const styles = StyleSheet.create({});
